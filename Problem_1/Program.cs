9ee907df9f5bf65bb084d6bf383798253b1c3e6b
Krishna R <rajamannar@hotmail.com>
2023-03-19 17:14:16 +0000

    solved problem 1

--- Problem_1/Program.cs
+++ Problem_1/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Problem_1
+﻿using System.ComponentModel;
+
+namespace Problem_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int limit = 1000;
+            int sum = 0;
+
+            //int multipleof3 = 0;
+            //int multipleof5 = 0;
+
+            //for (int k = 0; k < limit; k++) 
+            //{
+            //    multipleof3 = 3 * k;
+            //    multipleof5 = 5 * k;
+
+            //    if (multipleof3 > limit) 
+            //    {
+            //        multipleof3 = 0;
+            //    }
+
+            //    if (multipleof5 > limit)
+            //    {
+            //        multipleof5 = 0;
+            //    }
+
+            //    sum = sum + multipleof3 + multipleof5;
+            //}
+
+            for (int k = 0; k < limit; k++) 
+            {
+                if ((k % 3 == 0) || (k % 5 == 0)) 
+                {
+                    sum= sum+k;
+                }
+            }
+            Console.WriteLine(sum);
         }
     }
 }
